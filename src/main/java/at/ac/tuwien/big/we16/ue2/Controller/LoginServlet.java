f53c26c5e25fa012be3446b673aa648cab4a8032
Rares <e1328149@student.tuwien.ac.at>
2016-04-22 19:54:15 +0200

    Servlets + Beans

---
+++ src/main/java/at/ac/tuwien/big/we16/ue2/Controller/LoginServlet.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.big.we16.ue2.Controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Rares on 22-Apr-16.
+ */
+public class LoginServlet extends HttpServlet {
+
+}
---
+++ src/main/java/at/ac/tuwien/big/we16/ue2/Controller/OverviewServlet.java
@@ -0,0 +1,9 @@
+package at.ac.tuwien.big.we16.ue2.Controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Rares on 22-Apr-16.
+ */
+public class OverviewServlet extends HttpServlet {
+}
